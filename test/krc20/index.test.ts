c67d59a4a30d8089a1002260adc5fa63e5fb32c2
phongnhat19 <phongnhat19@gmail.com>
2021-05-19 10:52:06 +0700

    add unit test

--- test/krc20/index.test.ts
+++ test/krc20/index.test.ts
@@ -80,4 +80,16 @@ describe('KRC20 module test', () => {
     expect(tx.from).toEqual(ACCOUNT1.address);
     expect(tx.to).toEqual(TOKEN1.address);
   });
+
+  it('should return tx hash when transfer without `waitUntilMined` flag', async () => {
+    jest.setTimeout(150000);
+
+    const tx = await krc20Instance.transferRaw(
+      ACCOUNT1.privateKey,
+      ACCOUNT2.address,
+      '1000000000000000000',
+    );
+    expect(tx).toBeTruthy();
+    expect(typeof tx === 'string').toBeTruthy()
+  })
 });
