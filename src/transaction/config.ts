3e95dda46ac8fda6b11bc7fcd9aa8f71137e2da8
“phongnhat19” <phongnhat19@gmail.com>
2021-06-21 18:43:59 +0700

    add KARDIA_DEPLOYER to config

--- src/transaction/config.ts
+++ src/transaction/config.ts
@@ -1,4 +1,5 @@
 export const WAIT_TIMEOUT = 300000;
 export const DEFAULT_GAS = 900000;
 export const DEFAULT_GAS_PRICE = 1000000000;
-export const MAX_TX_GAS = 20000000;
+export const MAX_TX_GAS = 3000000;
+export const KARDIA_DEPLOYER = '0x14191195F9BB6e54465a341CeC6cce4491599ccC';
